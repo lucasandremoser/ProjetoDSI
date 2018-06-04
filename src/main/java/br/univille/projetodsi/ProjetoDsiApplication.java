f270eb83a28e2ae276281910ffbfe1c7563b229c
LUCAS ANDRÉ MOSER <lucasmoser@univille.br>
2018-06-04 20:00:42 -0300

    First Commit

---
+++ src/main/java/br/univille/projetodsi/ProjetoDsiApplication.java
@@ -0,0 +1,12 @@
+package br.univille.projetodsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDsiApplication.class, args);
+	}
+}
